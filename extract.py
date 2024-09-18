4d55fb29564ea53daaaebf2cf8d0471b00ac7c22
shettyshreyas-9 <shreyasshetty925@gmail.com>
2024-09-18 20:50:03 +0530

    created sample data and pushed it to google cloud bucket

--- extract.py
+++ extract.py
@@ -1,8 +1,19 @@
 from faker import Faker
 import random
 import string
+import os
+import csv
 
 from google.cloud import storage
+
+
+# try:
+#     SERVICE_ACCOUNT_KEY_PATH = '/opt/airflow/cred/gcp-test.json'
+#     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = SERVICE_ACCOUNT_KEY_PATH
+#     print(f"Service account key path set to: {SERVICE_ACCOUNT_KEY_PATH}")
+# except Exception as e:
+#     print(f"An error occurred while setting the service account key path: {e}")
+
 
 # Specify number of employees to generate
 num_employees = 10
@@ -15,22 +26,24 @@ employee_data= []
 # Define the character set for the password
 password_characters = string.ascii_letters + string.digits + 'm'
 
-for _ in range(num_employees):
-    employee={
-        "first_name": fake.first_name(),
-        "last_name": fake.last_name(),
-        "job_title": fake.job(),
-        "department": fake.job(),
-        "email": fake.email(),
-        "address": fake.address(),
-        "phone_number": fake.phone_number(),
-        "salary": fake.random_number(digits=5),
-        "password": "".join(random.choice(password_characters) for _ in range(8))
-    }
+# Generate employee data and save it to a CSV file
+with open('sample_test_1.csv', mode='w', newline='') as file:
+    fieldnames = ['first_name', 'last_name', 'job_title', 'department', 'email', 'address', 'phone_number', 'salary', 'password']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
 
-#     employee_data.append(employee)
-
-# print(employee_data)
+    writer.writeheader()
+    for _ in range(num_employees):
+        writer.writerow({
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "job_title": fake.job(),
+            "department": fake.job(),  # Generate department-like data using the job() method
+            "email": fake.email(),
+            "address": fake.city(),
+            "phone_number": fake.phone_number(),
+            "salary": fake.random_number(digits=5),  # Generate a random 5-digit salary
+            "password": ''.join(random.choice(password_characters) for _ in range(8))  # Generate an 8-character password with 'm'
+        })
 
 def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
     storage_client= storage.Client()
